621fb6483c34f306a2b5d465e15cb4d420fda429
Hugo Chinga G <hugo.chinga.g@gmail.com>
2023-05-11 11:56:30 -0400

    ljhlj

--- app/models/tar_factura.rb
+++ app/models/tar_factura.rb
@@ -17,7 +17,7 @@ class TarFactura < ApplicationRecord
 
 	def monto_pesos
 		unless self.uf.blank?
-			self.tar_facturaciones.map {|facturacion| facturacion.moneda == 'Pesos' ? facturacion.monto_pesos : (facturacion.monto_pesos * self.uf)}.compact.sum
+			self.tar_facturaciones.map {|facturacion| facturacion.monto_pesos}.compact.sum
 		else
 			0
 		end
