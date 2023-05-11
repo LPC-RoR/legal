11461bf0e7bf17eed7d7819b8d687d4893d7aab8
Hugo Chinga G <hugo.chinga.g@gmail.com>
2023-05-11 10:58:28 -0400

    ljlkjnl

--- app/models/tar_facturacion.rb
+++ app/models/tar_facturacion.rb
@@ -38,7 +38,7 @@ class TarFacturacion < ApplicationRecord
 
 	def uf_to_pesos(monto)
 		uf = TarUfSistema.find_by(fecha: self.fecha_uf)
-		uf.blank? ? 'blank' : (self.monto_ingreso / uf.valor)
+		uf.blank? ? 0 : (self.monto_ingreso / uf.valor)
 	end
 
 	def monto_pesos
