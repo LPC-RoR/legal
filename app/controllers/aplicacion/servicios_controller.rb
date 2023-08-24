c07e82261b647d8f2493a17b8e9e182f72e3405d
Hugo Chinga G <hugo.chinga.g@gmail.com>
2023-08-24 14:50:23 -0400

    jhgj

--- app/controllers/aplicacion/servicios_controller.rb
+++ app/controllers/aplicacion/servicios_controller.rb
@@ -1,5 +1,7 @@
 class Aplicacion::ServiciosController < ApplicationController
 
+  include Tarifas
+
   def aprobacion
     @objeto = TarAprobacion.find(params[:indice])
     init_tabla('tar_facturaciones', @objeto.tar_facturaciones, false)
