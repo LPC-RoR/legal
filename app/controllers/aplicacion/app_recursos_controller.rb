3c46a91db7fd2e00579f83341df41374d3551cfd
Hugo A. Chinga Guerrero <hugo.chinga.g@gmail.com>
2024-07-21 14:26:46 -0400

    lkjhjuhl

--- app/controllers/aplicacion/app_recursos_controller.rb
+++ app/controllers/aplicacion/app_recursos_controller.rb
@@ -1,6 +1,6 @@
 class Aplicacion::AppRecursosController < ApplicationController
-  before_action :authenticate_usuario!, only: %i[ home encuesta preguntas ]
-  before_action :scrty_on, only: %i[ home encuesta preguntas ]
+  before_action :authenticate_usuario!
+  before_action :scrty_on
   before_action :inicia_sesion, only: [:administracion, :procesos, :home]
 
   include Tarifas
