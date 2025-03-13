931f976cf27b328db3b7026b072c0023ce33ccac
Hugo A. Chinga Guerrero <hugo.chinga.g@gmail.com>
2025-03-13 13:16:16 -0300

    Arreglo del control de plazos

--- app/helpers/cptn_procs_helper.rb
+++ app/helpers/cptn_procs_helper.rb
@@ -85,6 +85,7 @@ module CptnProcsHelper
 		end
 	end
 
+	# Es neesario acceder al plazo sabiendo el código de la etapa
 	def etp_plz(ownr)
 		dnnc = ownr.dnnc
 		{
@@ -96,14 +97,18 @@ module CptnProcsHelper
 		}
 	end
 
+	def plz_ok?(fecha, plazo)
+		fecha.present? ? plazo.to_date >= fecha.to_date : (plazo.to_date > Time.zone.today.to_date ? nil : false)
+	end
+
 	def etp_plz_ok(ownr)
 		dnnc = ownr.dnnc
 		{
-			'etp_rcpcn'      => dnnc.fecha_trmtcn.blank? ? nil : dnnc.fecha_trmtcn <= dnnc.plz_trmtcn,
-			'etp_invstgcn'   => dnnc.fecha_trmn.blank? ? nil : dnnc.fecha_trmn <= dnnc.plz_invstgcn,
-			'etp_envio'      => dnnc.fecha_env_infrm.blank? ? nil : dnnc.fecha_env_infrm <= dnnc.plz_infrm,
-			'etp_prnncmnt'   => dnnc.fecha_prnncmnt.blank? ? nil : dnnc.fecha_prnncmnt <= dnnc.plz_prnncmnt,
-			'etp_mdds_sncns' => dnnc.fecha_prcsd.blank? ? nil : dnnc.fecha_prcsd <= dnnc.plz_mdds_sncns
+			'etp_rcpcn'      => plz_ok?(dnnc.fecha_trmtcn, dnnc.plz_trmtcn),
+			'etp_invstgcn'   => plz_ok?(dnnc.fecha_trmn, dnnc.plz_invstgcn),
+			'etp_envio'      => plz_ok?(dnnc.fecha_env_infrm, dnnc.plz_infrm),
+			'etp_prnncmnt'   => plz_ok?(dnnc.fecha_prnncmnt, dnnc.plz_prnncmnt),
+			'etp_mdds_sncns' => plz_ok?(dnnc.fecha_prcsd, dnnc.plz_mdds_sncns),
 		}
 	end
 
