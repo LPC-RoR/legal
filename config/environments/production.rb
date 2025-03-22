c30fff32a2113a9e501dea22f7cdb71b7b7767ca
Hugo A. Chinga Guerrero <hugo.chinga.g@gmail.com>
2025-03-22 12:47:49 -0300

    nueva prueba de configuración smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,9 +75,8 @@ Rails.application.configure do
     password: Rails.application.credentials[:smtp][:password],
     # No es necesario porque ya está encriptado a través de 465
 #    enable_starttls: true,
-    port: 465,
-    ssl:                  true,                         # Habilita SSL
-    enable_starttls_auto: false,                         # Desactiva STARTTLS automático
+    port: 587,
+    enable_starttls_auto: true,                         # Desactiva STARTTLS automático
     # :Login authentication encodes the password in base64
     authentication: :login
   }
