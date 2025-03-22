b3eb2e25912db5f4be45f796d3b3fa2346472ced
Hugo A. Chinga Guerrero <hugo.chinga.g@gmail.com>
2025-03-21 23:18:35 -0300

    ñlkjñlkjñ

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -5,5 +5,5 @@ class Usuario < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable, :lockable, :timeoutable, :confirmable
+         :recoverable, :rememberable, :validatable, :trackable, :lockable, :timeoutable
 end
