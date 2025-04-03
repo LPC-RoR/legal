2ff8d5022108868f23830240487b10af966e2cad
Hugo A. Chinga Guerrero <hugo.chinga.g@gmail.com>
2025-04-03 09:29:25 -0300

    kjñ ñlkjñl ñlkj

--- app/models/app_enlace.rb
+++ app/models/app_enlace.rb
@@ -1,6 +1,6 @@
 class AppEnlace < ApplicationRecord
 
-	belongs_to :ownr, polymorphic: true
+	belongs_to :ownr, polymorphic: true, optional:true
 
 	scope :gnrl, -> { where(ownr_id: nil) }
 	scope :dscrptn_ordr, -> { order(:descripcion) }
