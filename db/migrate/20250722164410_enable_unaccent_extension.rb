4bffaf27b4f2eacbe256f0e0762ae7be2236b440
Hugo A. Chinga Guerrero <hugo.chinga.g@gmail.com>
2025-07-22 13:01:47 -0400

    lkjñlKJ

--- db/migrate/20250722164410_enable_unaccent_extension.rb
+++ db/migrate/20250722164410_enable_unaccent_extension.rb
@@ -1,5 +1,14 @@
 class EnableUnaccentExtension < ActiveRecord::Migration[8.0]
   def change
-    enable_extension "unaccent"
+    # Intenta crear la extensión pero continúa si falla
+    execute <<-SQL
+      DO $$
+      BEGIN
+        CREATE EXTENSION IF NOT EXISTS unaccent;
+      EXCEPTION WHEN insufficient_privilege THEN
+        RAISE NOTICE 'No se pudo crear la extensión unaccent: permisos insuficientes';
+      END
+      $$;
+    SQL
   end
 end
