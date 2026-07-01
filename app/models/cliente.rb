fd9b8ed198ef86165741ee0ce7115aeaa8d6d733
Hugo Chinga G. <hugo.chinga.g@gmail.com>
2026-06-30 20:45:44 -0400

    Version con tar_aprobacion_id

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -65,7 +65,7 @@ class Cliente < ApplicationRecord
 		  "(ownr_type = 'TarCalculo' AND ownr_id IN (?)) OR (ownr_type = 'Asesoria' AND ownr_id IN (?))",
 		  tar_calculo_ids,
 		  asesoria_ids
-		).where(cli_aprobacion_id: nil)
+		).where(cli_aprobacion_id: nil, tar_aprobacion_id: nil)
 	end
 
     # ---------------------------------------------------------------- ESTADOS con AASM
