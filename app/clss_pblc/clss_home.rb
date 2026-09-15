07544f5092cf09edde5c34dc9c6a08071af093e2
Hugo Chinga G. <hugo.chinga.g@gmail.com>
2026-09-14 21:31:55 -0300

    SEO básico

--- app/clss_pblc/clss_home.rb
+++ app/clss_pblc/clss_home.rb
@@ -5,7 +5,8 @@ class ClssHome < ApplicationRecord
 			metodologia: 	'Etapas y plazos del procedimiento de investigación | Laborsafe',
 			laborsafe: 		'Plataforma tecnológica para investigaciones Ley 21.643 | Laborsafe',
 			equipo: 		'Equipo legal: abogados especialistas en Ley 21.643 | Laborsafe',
-			simulador: 		'Simulador de plazos del procedimiento Ley 21.643 | Laborsafe'
+			simulador: 		'Simulador de plazos del procedimiento Ley 21.643 | Laborsafe',
+			registro: 		'Cuenta demo gratuita: evalúe Laborsafe por 10 días'
 		}.freeze
 	end
 end
